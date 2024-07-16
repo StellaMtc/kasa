57087041143c67c3de401ea03ce2ad735f1f8423
Estelle Mitchovitch <e.m2017@outlook.fr>
2024-07-16 09:57:45 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './components/Header/'
-import Footer from './components/Footer/'
-import Home from './pages/Home/'
-import Apropos from './pages/Apropos/'
-import Estate from './pages/Estate'
-import Error from './pages/Error'
-import './index.scss'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./styles/index.scss";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import Article from "./pages/Article";
+import APropos from "./pages/APropos";
+import PageError from "./pages/PageError";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
+function App() {
+  return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/a-propos" element={<Apropos />} />
-        <Route path="/estate/:id" element={<Estate />} />
-        <Route path="*" element={<Error />} />
+        <Route path="/Kasa/" element={<Home />} />
+        <Route path="/Kasa/logements/:id" element={<Article />} />
+        <Route path="/Kasa/a-propos" element={<APropos />} />
+        <Route path="/kasa/*" element={<PageError />} />
       </Routes>
       <Footer />
     </Router>
-  </React.StrictMode>
-)
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(<App />);
