2903ae50593a53f01d2a505a063c0b2a3b3f9e7e
Estelle Mitchovitch <e.m2017@outlook.fr>
2024-07-16 12:54:46 +0200

    first commit

--- src/pages/APropos.js
+++ src/pages/APropos.js
@@ -12,7 +12,7 @@ function APropos() {
 
       <Collapse title="Respect" content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
 
-      <Collapse title="Service" content="Kasa s'engage à fournir un service de qualité à ses clients. Notre équipe est disponible 7j/7 pour répondre à vos questions et vous aider à trouver le logement idéal." />
+      <Collapse title="Service" content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
 
       <Collapse title="Sécurité" content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
 
